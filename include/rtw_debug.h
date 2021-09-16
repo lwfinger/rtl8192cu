0483e0f2d91a92abd7e209069d6f81b68a27a214
Alex Renn <alexander.renn@securekey.com>
2021-09-16 10:48:46 +0300

    removed debug (DBG_8192C, RT_TRACE)

--- include/rtw_debug.h
+++ include/rtw_debug.h
@@ -192,7 +192,7 @@ extern void rtl871x_cedbg(const char *fmt, ...);
 #endif
 
 #endif /* CONFIG_DEBUG_RTL871X */
-
+#undef _dbgdump
 
 #if	defined (_dbgdump) && defined (_MODULE_DEFINE_)
 
@@ -206,6 +206,8 @@ extern void rtl871x_cedbg(const char *fmt, ...);
 		}while(0)
 
 #endif
+#undef RT_TRACE
+#define RT_TRACE(_Comp, _Level, Fmt) do{}while(0)
 
 
 #if	defined (_dbgdump)
@@ -273,6 +275,7 @@ extern void rtl871x_cedbg(const char *fmt, ...);
 #endif
 
 #endif /* CONFIG_DEBUG_RTL819X */
+#undef	_dbgdump
 
 
 #ifdef PLATFORM_WINDOWS
